5e67847fcbf5d10e8177c847efdc16e9975bb994
plainnany <plainnany@gmail.com>
2019-07-16 22:47:55 +0800

    添加一个button示例

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,5 @@
-console.log('hello world webpack')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from './button';
+
+ReactDOM.render(<Button></Button>, document.body);
