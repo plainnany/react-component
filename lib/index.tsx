c3faa65ba87f7b69a639261f20a882ad5e6d4c9c
plainnany <plainnany@gmail.com>
2019-07-23 21:51:19 +0800

    修改配置项，发布第一个npm包

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,2 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Icon from './icon/icon'
 
-ReactDOM.render(<div>
-  <Icon name="wechat" className="test"></Icon>
-</div>, document.getElementById('root'))
+export { default as Icon } from './icon/icon'
